8cfa719a6d7eabec653cf2c0479260f392df702b
rlyshw <rlyshw@gmail.com>
2023-04-22 17:45:28 -0400

    MVP with blocking POST working

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is a really basic content-script chrome extension that will block Twitter Blue users as they load onto your TL.
+
+
+schema/mock will have model json dumps pulled from a live tl object.
+    tweet.json is a model schema of just one tweet object
+
+quick and dirty front-end method to click all the unblock buttons
+`document.querySelectorAll('[aria-label="Blocked"]').forEach((button)=>button.click())`
--- content-script.js
+++ content-script.js
@@ -1,89 +1,41 @@
-var verified_list = []
-/* this whole section was using a naive HTML parse approach. Not ideal, HTML is easier for TWIT to change... */
-
-// parentWalk = (element)=>{ //recursively walk up the parentElement tree to find the data we need
-//   if(!element) return; //safety check...
-//   var parent = element.parentElement
-//   if(element.nodeName == 'A'){ //it's a link to the profile
-//     username=element.getAttribute('href').slice(1)
-//     if(!verified_list.includes(username))
-//       verified_list.push(username);
-//     return;
-//   }
-//   else if(element.nodeName=='ARTICLE') { //we've gone all the way up to the parent tweet
-//     console.log('hit the top tweet', element) //need to go back down to find the embedded check...
-//     return 
-//   } else if(element.getAttribute('data-testid') == 'User-Name') {
-//     username=element.querySelectorAll('span')[3].innerHTML.slice(1)
-//     if(!verified_list.includes(username))
-//       verified_list.push(username)
-//     return
-//   }
-//   else {
-//     parentWalk(parent)
-//   }
+// this is the HTML parsing method.
+// NOT using this because you can't easily extrapolate USER_ID from the UI.
+//   might revisit, do a "click the three buttons, click the block button, OK OK"
+// findVerified = () => { //finds blue checkmarks
+//   var verified_checks = document.querySelectorAll('[aria-label="Verified account"]')
+//   verified_checks.forEach((check)=>{
+//     if(check.firstChild.childElementCount==1 && !check.firstChild.firstChild.hasAttribute('fill'))
+//       parentWalk(check.parentElement)
+//   })
 // }
 
-findVerified = () => { //finds blue checkmarks
-  var verified_checks = document.querySelectorAll('[aria-label="Verified account"]')
-  verified_checks.forEach((check)=>{
-    if(check.firstChild.childElementCount==1 && !check.firstChild.firstChild.hasAttribute('fill'))
-      parentWalk(check.parentElement)
-  })
-  // console.log(verified_list)
-}
-
-setInterval(()=>findVerified(),5000) // this is the naive HTML parsing/long polling approach
+// setInterval(()=>findVerified(),5000) // this is the naive HTML parsing/long polling approach
 /* end the HTML parsing method */
 
 /* XHR intercept / timeline object method */
 var rawOpen = XMLHttpRequest.prototype.open;
+
 //this part hooks into the XHRs so we can grab the TL Object
 //mostly taken from SO https://stackoverflow.com/questions/16959359/intercept-xmlhttprequest-and-modify-responsetext
 XMLHttpRequest.prototype.open = function() { 
-
 	if (!this._hooked) {
 		this._hooked = true;
-		setupHook(this);
+		setupOpenHook(this);
 	}
 	rawOpen.apply(this, arguments);
 }
-
-
-//functions to walk into the TL object and find blue checks
-function parseHomeTL(hometlobj){
-  tweets = hometlobj.data.home.home_timeline_urt.instructions[0].entries //dig for the tweet list
-
-  tweets.forEach((tweet)=>{ // iterate through tweets on the TL object
-    if(tweet.content.entryType == "TimelineTimelineItem"){ //weird entryType name...
-      if(tweet,tweet.content.itemContent.tweet_results.result.core){ // some validation since the entries are so inconsistent
-        user = tweet.content.itemContent.tweet_results.result.core.user_results.result //honestly this object is gnarly
-        if(user.is_blue_verified && user.profile_image_shape!="Square") // use !="Square" to avoid "official"/business accounts
-          console.log(user.legacy.screen_name,user) //should spit out blue check users. 
-
-          //TODO: from here, take the blue check user object and fire off the block request
-      }
-    }
-  })
-  console.log(tweets) //dump the tweet list for debugging
-}
-//hook into the update req. Is this formatted differently?
-function parseHomeLatestTL(homelatesttlobj){
-  console.log(homelatesttlobj.data.home.home_timeline_urt.instructions[0].entries)
-}
-
-
 //the rest of the SO XHR hook
-function setupHook(xhr) {
+function setupOpenHook(xhr) { //TODO: Clean this up, don't actually need to hook for any mods
 	function getter() {
 		delete xhr.responseText;
 		var ret = xhr.responseText;
-    if(xhr.responseURL.includes("HomeTimeline")) // HomeTimeline request, brings us the timeline object
+    // console.log("log xhr and xhr.response",xhr,JSON.parse(xhr.response))
+    if(xhr.responseURL.includes("HomeTimeline") || xhr.responseURL.includes("HomeLatestTimeLine")) // HomeTimeline request, brings us the timeline object
       parseHomeTL(JSON.parse(xhr.response))
-    if(xhr.responseURL.includes("HomeLatestTimeline")){ // maybe updates are different?
-      parseHomeLatestTL(JSON.parse(xhr.response))
-      xhr.onprgress = console.log
+    if(xhr.responseURL.includes("BlockedAccounts")){
+      getBlockedList(JSON.parse(xhr.response))
     }
+    
 		setup();
 		return ret;
 	}
@@ -102,3 +54,57 @@ function setupHook(xhr) {
 	setup();
 }
 
+var rawSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
+var modHeaders = {
+  "accept": "*/*",
+  "accept-language": "en-US,en;q=0.9,de;q=0.8",
+  "authorization": "",
+  "cache-control": "no-cache",
+  "content-type": "application/x-www-form-urlencoded",
+  "pragma": "no-cache",
+  "x-csrf-token": "",
+  "x-twitter-active-user": "yes",
+  "x-twitter-auth-type": "OAuth2Session",
+  "x-twitter-client-language": "en"
+}
+XMLHttpRequest.prototype.setRequestHeader = function() {
+  if(arguments[0]=="authorization") modHeaders.authorization = arguments[1]
+  if(arguments[0]=="x-csrf-token") modHeaders["x-csrf-token"] = arguments[1]
+  rawSetRequestHeader.apply(this,arguments)
+}
+
+//functions to walk into the TL object and find blue checks
+function parseHomeTL(hometlobj){
+  tweets = hometlobj.data.home.home_timeline_urt.instructions[0].entries //dig for the tweet list
+
+  tweets.forEach((tweet)=>{ // iterate through tweets on the TL object
+    if(tweet.content.entryType == "TimelineTimelineItem"){ //weird entryType name...
+      if(tweet,tweet.content.itemContent.tweet_results.result.core){ // some validation since the entries are so inconsistent
+        user = tweet.content.itemContent.tweet_results.result.core.user_results.result //honestly this object is gnarly
+        if(user.is_blue_verified && user.profile_image_shape!="Square"){ // use !="Square" to avoid "official"/business accounts
+          console.log(user.legacy.screen_name,user) //should spit out blue check users. 
+          block(user.rest_id)
+        }
+      }
+    }
+  })
+  console.log("log tl tweet list",tweets) //dump the tweet list for debugging
+}
+
+
+getBlockedList = (data) => {
+  console.log(data)
+}
+
+// this is the xhr that kicks off a block
+// I think a lot of these headers are probably required. Need to scrape them off other requests from the XHR hook
+block = (userId) => {
+  console.log(userId)
+  fetch("https://twitter.com/i/api/1.1/blocks/create.json", {
+    "headers": modHeaders,
+    "body": "user_id="+userId,
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "include"
+  }).then(console.log);
+}
