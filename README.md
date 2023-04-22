b09ae4b151094bd902dda1692e2647a4ae14b868
rlyshw <rlyshw@gmail.com>
2023-04-22 15:21:42 -0400

    some documentation

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a really basic content-script chrome extension that will block Twitter Blue users as they load onto your TL.
--- content-script.js
+++ content-script.js
@@ -1,26 +1,28 @@
 var verified_list = []
-parentWalk = (element)=>{ //recursively walk up the parentElement tree to find the data we need
-  if(!element) return; //safety check...
-  var parent = element.parentElement
-  if(element.nodeName == 'A'){ //it's a link to the profile
-    username=element.getAttribute('href').slice(1)
-    if(!verified_list.includes(username))
-      verified_list.push(username);
-    return;
-  }
-  else if(element.nodeName=='ARTICLE') { //we've gone all the way up to the parent tweet
-    console.log('hit the top tweet', element) //need to go back down to find the embedded check...
-    return 
-  } else if(element.getAttribute('data-testid') == 'User-Name') {
-    username=element.querySelectorAll('span')[3].innerHTML.slice(1)
-    if(!verified_list.includes(username))
-      verified_list.push(username)
-    return
-  }
-  else {
-    parentWalk(parent)
-  }
-}
+/* this whole section was using a naive HTML parse approach. Not ideal, HTML is easier for TWIT to change... */
+
+// parentWalk = (element)=>{ //recursively walk up the parentElement tree to find the data we need
+//   if(!element) return; //safety check...
+//   var parent = element.parentElement
+//   if(element.nodeName == 'A'){ //it's a link to the profile
+//     username=element.getAttribute('href').slice(1)
+//     if(!verified_list.includes(username))
+//       verified_list.push(username);
+//     return;
+//   }
+//   else if(element.nodeName=='ARTICLE') { //we've gone all the way up to the parent tweet
+//     console.log('hit the top tweet', element) //need to go back down to find the embedded check...
+//     return 
+//   } else if(element.getAttribute('data-testid') == 'User-Name') {
+//     username=element.querySelectorAll('span')[3].innerHTML.slice(1)
+//     if(!verified_list.includes(username))
+//       verified_list.push(username)
+//     return
+//   }
+//   else {
+//     parentWalk(parent)
+//   }
+// }
 
 findVerified = () => { //finds blue checkmarks
   var verified_checks = document.querySelectorAll('[aria-label="Verified account"]')
@@ -28,14 +30,18 @@ findVerified = () => { //finds blue checkmarks
     if(check.firstChild.childElementCount==1 && !check.firstChild.firstChild.hasAttribute('fill'))
       parentWalk(check.parentElement)
   })
-  console.log(verified_list)
+  // console.log(verified_list)
 }
 
-setInterval(()=>findVerified(),5000)
+setInterval(()=>findVerified(),5000) // this is the naive HTML parsing/long polling approach
+/* end the HTML parsing method */
 
+/* XHR intercept / timeline object method */
 var rawOpen = XMLHttpRequest.prototype.open;
+//this part hooks into the XHRs so we can grab the TL Object
+//mostly taken from SO https://stackoverflow.com/questions/16959359/intercept-xmlhttprequest-and-modify-responsetext
+XMLHttpRequest.prototype.open = function() { 
 
-XMLHttpRequest.prototype.open = function() {
 	if (!this._hooked) {
 		this._hooked = true;
 		setupHook(this);
@@ -43,31 +49,38 @@ XMLHttpRequest.prototype.open = function() {
 	rawOpen.apply(this, arguments);
 }
 
-function parseHomeTL(hometlobj){
-  tweets = hometlobj.data.home.home_timeline_urt.instructions[0].entries
 
-  tweets.forEach((tweet)=>{
-    if(tweet.content.entryType == "TimelineTimelineItem"){
-      if(tweet,tweet.content.itemContent.tweet_results.result.core){
-        user = tweet.content.itemContent.tweet_results.result.core.user_results.result
-        if(user.is_blue_verified && user.profile_image_shape!="Square") console.log(user.legacy.screen_name,user)
+//functions to walk into the TL object and find blue checks
+function parseHomeTL(hometlobj){
+  tweets = hometlobj.data.home.home_timeline_urt.instructions[0].entries //dig for the tweet list
+
+  tweets.forEach((tweet)=>{ // iterate through tweets on the TL object
+    if(tweet.content.entryType == "TimelineTimelineItem"){ //weird entryType name...
+      if(tweet,tweet.content.itemContent.tweet_results.result.core){ // some validation since the entries are so inconsistent
+        user = tweet.content.itemContent.tweet_results.result.core.user_results.result //honestly this object is gnarly
+        if(user.is_blue_verified && user.profile_image_shape!="Square") // use !="Square" to avoid "official"/business accounts
+          console.log(user.legacy.screen_name,user) //should spit out blue check users. 
+
+          //TODO: from here, take the blue check user object and fire off the block request
       }
     }
   })
-  console.log(tweets)
+  console.log(tweets) //dump the tweet list for debugging
 }
+//hook into the update req. Is this formatted differently?
 function parseHomeLatestTL(homelatesttlobj){
   console.log(homelatesttlobj.data.home.home_timeline_urt.instructions[0].entries)
 }
 
+
+//the rest of the SO XHR hook
 function setupHook(xhr) {
 	function getter() {
 		delete xhr.responseText;
 		var ret = xhr.responseText;
-    console.log(xhr.responseURL)
-    if(xhr.responseURL.includes("HomeTimeline"))
+    if(xhr.responseURL.includes("HomeTimeline")) // HomeTimeline request, brings us the timeline object
       parseHomeTL(JSON.parse(xhr.response))
-    if(xhr.responseURL.includes("HomeLatestTimeline")){
+    if(xhr.responseURL.includes("HomeLatestTimeline")){ // maybe updates are different?
       parseHomeLatestTL(JSON.parse(xhr.response))
       xhr.onprgress = console.log
     }
@@ -76,7 +89,7 @@ function setupHook(xhr) {
 	}
 
 	function setter(str) {
-		console.log('set responseText: %s', str);
+		console.log('set responseText: %s', str); //not using this, initial SO code was for intercepting/rewriting req/responses
 	}
 
 	function setup() {
@@ -89,15 +102,3 @@ function setupHook(xhr) {
 	setup();
 }
 
-// fetch("https://twitter.com/i/api/graphql/7JUXeanO9cYvjKvaPe7EMg/HomeTimeline", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "en-US,en;q=0.9,de;q=0.8",
-//     "content-type": "application/json",
-//   },
-//   "referrer": "https://twitter.com/home",
-//   "referrerPolicy": "strict-origin-when-cross-origin",
-//   "method": "POST",
-//   "mode": "cors",
-//   "credentials": "include"
-// }).then(console.log)
